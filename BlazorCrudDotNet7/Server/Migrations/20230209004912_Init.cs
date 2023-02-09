0cf617a41608cf21a1315727bbce4409b3c59f55
fangqiank <fangqiank@yahoo.com>
2023-02-09 08:55:13 +0800

    Add project files.

---
+++ BlazorCrudDotNet7/Server/Migrations/20230209004912_Init.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BlazorCrudDotNet7.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Category = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Books", 9.99m, "The Hitchhiker's Guide to the Galaxy" },
+                    { 2, "Books", 12.99m, "Ready Player One" },
+                    { 3, "Movies", 4.99m, "Back to the Future" },
+                    { 4, "Movies", 3.99m, "Toy Story" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
